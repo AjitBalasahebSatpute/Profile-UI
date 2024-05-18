b0728641134104b86e1688df67f9969ffd63f7bb
Ajit Satpute <ajitsatpute1999@gmail.com>
2024-05-18 18:55:15 +0530

    updated profile with all details

--- AjitSatpute-Profile/src/app/app.component.ts
+++ AjitSatpute-Profile/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AjitSatpute-Profile';
+  menu!:any;
+  icon!:any;
+  ngOnInit(): void {
+  
+      
+  }
+ toggleMenu(){
+  console.log("clicked")
+     this.menu= document.querySelector(".menu-links");
+     this.icon= document.querySelector(".hamburger-icon");
+    this.menu.classList.toggle("open")
+    this.icon.classList.toggle("open")
+}
 }
--- AjitSatpute-Profile/src/app/home/home.component.ts
+++ AjitSatpute-Profile/src/app/home/home.component.ts
@@ -7,13 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  menu!:any;
-  icon!:any;
- toggleMenu(){
-     this.menu= document.querySelector(".menu-links");
-     this.icon= document.querySelector(".hamburger-icon");
-    this.menu.classList.toggle("open")
-    this.icon.classList.toggle("open")
-}
+
 
 }
